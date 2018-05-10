7129858063cbee7a26073d94554aeb60822e3f28
Denis Storti Da Silva <denstorti@users.noreply.github.com>
2018-05-10 00:58:04 -0400

    Initial commit

---
+++ AudioType.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    public class AudioType
+    {
+        public static AudioTypeEnum CheckAudioType(byte[] audio)
+        {
+            //30 26 B2 75 8E 66 CF 11 A6 D9 00 AA 00 62 CE 6C
+            if (audio[0] == 0x30 && audio[1] == 0x26 && audio[2] == 0xB2 && audio[3] == 0x75 && audio[4] == 0x8E && audio[5] == 0x66 && audio[6] == 0xCF && audio[7] == 0x11 && audio[8] == 0xA6 && audio[9] == 0xD9 &&
+                audio[10] == 0x00 && audio[11] == 0xAA && audio[12] == 0x00 && audio[13] == 0x62 && audio[14] == 0xCE && audio[15] == 0x6C)
+
+            {
+                return AudioTypeEnum.WMA;
+            }
+            else if ((audio[0] == 0xFF) || //255 - Exception for Vivo project 24/04/2018
+                (audio[0] == 0x49 && audio[1] == 0x44 && audio[2] == 0x33) ||  //or 73 68 51 
+                (audio[0] == 0xFF && audio[1] == 0xFB))  //or 255 251
+            {
+                return AudioTypeEnum.MP3;
+            }
+            else if (audio[0] == 0x52 && audio[1] == 0x49 && audio[2] == 0x46 && audio[3] == 0x46) //WAV FILE - 52 49 46 46
+            {
+                return AudioTypeEnum.WAV;
+            }
+            else
+            {
+                return AudioTypeEnum.Unknown;
+            }
+        }
+
+        
+    }
+
+    public enum AudioTypeEnum
+    {
+        Unknown,
+        WAV,
+        MP3,
+        WMA
+    }
+}
---
+++ CustomAudioConverter.cs
@@ -0,0 +1,185 @@
+﻿using NAudio.Utils;
+using NAudio.Wave;
+using System;
+using System.IO;
+
+namespace Converter
+{
+    public class CustomAudioConverter : IAudioConverter
+    {
+
+        public byte[] CustomConversionToLinear(byte[] audio)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            bool resample8KHz16bit = false;
+
+            MemoryStream inMem = new MemoryStream(audio);
+
+            byte[] output;
+            int outputLength = 0;
+
+            String traceabilityId = Guid.NewGuid().ToString() + ": ";
+            log.Info(traceabilityId + "Start CustomConversionToLinear with audio[" + audio.Length + "]. resample8KHz16bit = " + resample8KHz16bit);
+            log.Info(traceabilityId + "First bytes of audio: " + audio[0] + " " + audio[1] + " " + audio[2] + " " + audio[3]);
+
+            switch (AudioType.CheckAudioType(audio))
+            {
+                case AudioTypeEnum.WAV:
+                    log.Info(traceabilityId + "File is WAV");
+
+                    if (!resample8KHz16bit)
+                    {
+                        output = inMem.ToArray();
+                        outputLength = output.Length;
+                    }
+                    else
+                    {
+                        byte[] fileBytes;
+                        using (var wfr = new WaveFileReader(inMem))
+                        {
+                            using (var pcmStream = new WaveFormatConversionStream(new WaveFormat(8000, 16, wfr.WaveFormat.Channels), wfr))
+                            {
+                                fileBytes = new byte[pcmStream.Length];
+                                pcmStream.Read(fileBytes, 0, Convert.ToInt32(pcmStream.Length));
+                                output = fileBytes;
+                                outputLength = output.Length;
+
+                            }
+                        }
+                    }
+                    break;
+
+                case AudioTypeEnum.MP3:
+                    log.Info(traceabilityId + "File is MP3");
+                    output = ConvertMP3toWAV(inMem, resample8KHz16bit);
+                    outputLength = output.Length;
+                    break;
+
+                case AudioTypeEnum.WMA:
+                    log.Info(traceabilityId + "File is WMA");
+                    output = ConvertWMAtoWAV(inMem, resample8KHz16bit);
+                    outputLength = output.Length;
+                    break;
+
+                default:
+                case AudioTypeEnum.Unknown:
+                    log.Warn(traceabilityId + "File is UNKOWN");
+
+                    output = inMem.ToArray();
+                    outputLength = output.Length;
+                    break;
+            }
+            
+            log.Info(traceabilityId + "Finished: output length is " + outputLength);
+            return output;
+        }
+
+        public static byte[] ConvertMP3toWAV(MemoryStream inMem, bool resample8KHz16bit = false)
+        {
+            /* its a mp3*/
+            MemoryStream outMem = new MemoryStream();
+
+            using (var reader = new Mp3FileReader(inMem))
+            {
+                if (!resample8KHz16bit)
+                {
+                    WriteWavFileToStream(outMem, reader);
+                    return outMem.ToArray();
+                }
+                else
+                {
+                    var outFormat = new WaveFormat(8000, 16, reader.WaveFormat.Channels);
+
+                    using (var resampler = new MediaFoundationResampler(reader, outFormat))
+                    {
+                        // resampler.ResamplerQuality = 60;
+                        IWaveProvider sourceProvider = (IWaveProvider)resampler;
+                        long outputLength = 0;
+                        var buffer = new byte[sourceProvider.WaveFormat.AverageBytesPerSecond * 4];
+                        while (true)
+                        {
+                            int bytesRead = sourceProvider.Read(buffer, 0, buffer.Length);
+                            if (bytesRead == 0)
+                            {
+                                // end of source provider
+                                break;
+                            }
+                            outputLength += bytesRead;
+                            if (outputLength > Int32.MaxValue)
+                            {
+                                throw new InvalidOperationException("WAV File cannot be greater than 2GB. Check that sourceProvider is not an endless stream.");
+                            }
+                            outMem.Write(buffer, 0, bytesRead);
+                        }
+
+                        return outMem.ToArray();
+                    }
+
+                }
+            }
+        }
+
+        //this method is present on NAudio 1.8+, but it is not in 1.7. In order to maintain compatibility with SS11.2 it was copied here from NAudio source code.
+        public static void WriteWavFileToStream(Stream outStream, IWaveProvider sourceProvider)
+        {   
+            using (var writer = new WaveFileWriter(new IgnoreDisposeStream(outStream), sourceProvider.WaveFormat))
+            {
+                var buffer = new byte[sourceProvider.WaveFormat.AverageBytesPerSecond * 4];
+                while (true)
+                {
+                    var bytesRead = sourceProvider.Read(buffer, 0, buffer.Length);
+                    if (bytesRead == 0)
+                    {
+                        // end of source provider
+                        outStream.Flush();
+                        break;
+                    }
+
+                    writer.Write(buffer, 0, bytesRead);
+                }
+            }
+        }
+
+        /// <summary>
+        /// PROBLEMS WITH THE NEW VERSION OF WMA DOES NOT MAKE THIS CODE WORK. THERE IS NO CONSTRUCTOR WITH STREAM INPUT in NAUDIO.WMA
+        /// </summary>
+        /// <param name="audio"></param>
+        /// <returns></returns>
+        public static byte[] ConvertWMAtoWAV(MemoryStream inMem, bool resample8KHz16bit = false)
+        {
+            /* its a wma */
+            //MemoryStream outMem = new MemoryStream();
+            //using (var reader = new WMAFileReader(inMem))
+            //{
+            //    var outFormat = new WaveFormat(8000, 16, 1);
+            //    using (var resampler = new MediaFoundationResampler(reader, outFormat))
+            //    {
+            //        // resampler.ResamplerQuality = 60;
+            //        IWaveProvider sourceProvider = (IWaveProvider)resampler;
+            //        long outputLength = 0;
+            //        var buffer = new byte[sourceProvider.WaveFormat.AverageBytesPerSecond * 4];
+            //        while (true)
+            //        {
+            //            int bytesRead = sourceProvider.Read(buffer, 0, buffer.Length);
+            //            if (bytesRead == 0)
+            //            {
+            //                // end of source provider
+            //                break;
+            //            }
+            //            outputLength += bytesRead;
+            //            if (outputLength > Int32.MaxValue)
+            //            {
+            //                throw new InvalidOperationException("WAV File cannot be greater than 2GB. Check that sourceProvider is not an endless stream.");
+            //            }
+            //            outMem.Write(buffer, 0, bytesRead);
+            //        }
+
+            //        return outMem.ToArray();
+            //    }
+            //}
+
+            throw new NotImplementedException();
+        }
+    }
+}
